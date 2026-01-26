d7a9132e2c10edf5d8b63ab137b68ab1b2994b12
viscitotraslochi <viscitotraslochi@gmail.com>
2026-01-26 11:41:55 +0100

    asd

--- server/db.js
+++ server/db.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL.includes('supabase.co') 
-    ? { rejectUnauthorized: false } 
-    : false
+  ssl: {
+    rejectUnauthorized: false
+  },
+
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 module.exports = pool;
