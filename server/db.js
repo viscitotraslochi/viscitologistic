45f24cb667cef174a2a7e795e044b6e9e79aadb6
viscitotraslochi <viscitotraslochi@gmail.com>
2026-01-26 11:46:59 +0100

    asd

--- server/db.js
+++ server/db.js
@@ -4,12 +4,9 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  },
-
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
+    // Questo è il comando magico per risolvere "self-signed certificate"
+    rejectUnauthorized: false 
+  }
 });
 
 module.exports = pool;
