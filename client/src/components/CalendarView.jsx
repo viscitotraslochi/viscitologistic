2c5019295fab9ab38d5bc598f40707ca21650eea
viscitotraslochi <viscitotraslochi@gmail.com>
2026-01-28 19:21:55 +0100

    SEO

--- client/src/components/CalendarView.jsx
+++ client/src/components/CalendarView.jsx
@@ -4,7 +4,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list'; 
-import itLocale from '@fullcalendar/core/locales/it'; 
+import itLocale from '@fullcalendar/core/locales/it.js';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
